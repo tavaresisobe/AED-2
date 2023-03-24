01e0d8417731a08d939ee806f4803b4d948df2c5
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2023-03-23 22:31:40 -0300

    Update ap1_recursao.c

--- ap1_recursao.c
+++ ap1_recursao.c
@@ -6,32 +6,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 // ###################### Funcoes e Procedimentos do programa ##############
 
 void start() ;
-void busca() ;
+int busca() ;
 
 void start()
 {
   int n, num ;
   scanf ("%d", &n) ;
-  int *vet ;
+  int *vet, count = 0 ;
   vet = malloc (n * sizeof(int)) ;
   for (int i = 0 ; i < n ; i++)
   {
       scanf ("%d", &vet[i]) ;
   }
   scanf ("%d", &num) ;
-  busca (vet,num,n) ;
+  busca (vet,num,n, &count) ;
+  printf ("Numero de chamadas recursivas: %d\n", count) ;
 }
 
-void busca(int *vet, int num, int n)
+int busca(int *vet, int num, int n, int *count)
 {
-  for (int i = 0 ; i < n ; i++)
-  {
-    printf ("%d ", vet[i]) ;  
-  }
+  int i = (n+(n/2))/2 ;
+  return 0 ;
 }
 
 // ############################ Principal ###################################
