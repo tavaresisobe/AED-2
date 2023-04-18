75bc05a8136381d41a622c03db0fe5f6dae065d3
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2023-04-18 03:40:14 -0300

    Update ap4_quicksort.c

--- ap4_quicksort.c
+++ ap4_quicksort.c
@@ -1,507 +1,99 @@
 #include <stdio.h>
-#include <stdlib.h> // srand, rand
-#include <time.h> // time
+#include <stdlib.h>
 
-
-void start ()
-{
-    int tam_vet, i ;
-    int *vet, *vet2 ;
-    scanf ("%d", &tam_vet) ;
-    vet = malloc (tam_vet * sizeof(int)) ;
-    for (i = 0 ; i < tam_vet ; i++)
-    {
-        scanf ("%d", &vet[i]) ;
-    }
-    vet2 = malloc (tam_vet * sizeof(vet)) ;
-    for (i = 0 ; i < tam_vet ; i++) //copiando vetor para implementação do quicksort utiliziando a mediana
-    {
-        vet2[i] = vet[i] ;
-    }
-    
-    
-//	srand(time(NULL));
-	quick_sort(vet, 0, tam_vet - 1);
-	quick_sort(vet2, 0, tam_vet - 1);
-	
-	for(i = 0; i < tam_vet; i++)
-	    printf("%d ", vet[i]);
-	printf ("\n\n") ;
-	for(i = 0; i < tam_vet; i++)
-	    printf("%d ", vet2[i]);
-}
-
-// função que realiza a troca entre dois elementos
-void troca(int vet[], int i, int j)
-{
-	int aux = vet[i];
-	vet[i] = vet[j];
-	vet[j] = aux;
-}
-
-// particiona e retorna o índice do pivô
-int particiona(int vet[], int inicio, int fim)
-{
-	int pivo, pivo_indice, i;
-	
-	pivo = vet[fim]; // o pivô é sempre o último elemento
-	pivo_indice = inicio;
-	
-	for(i = inicio; i < fim; i++)
-	{
-		// verifica se o elemento é <= ao pivô
-		if(vet[i] <= pivo)
-		{
-			// realiza a troca
-			troca(vet, i, pivo_indice);
-			// incrementa o pivo_indice
-			pivo_indice++;
-		}
-	}
-	
-	// troca o pivô
-	troca(vet, pivo_indice, fim);
-	
-	// retorna o índice do pivô
-	return pivo_indice;
-}
-
-// escolhe um pivô aleatório para evitar o pior caso do quicksort
-int particiona_random(int vet[], int inicio, int fim)
-{
-	// seleciona um número entre fim (inclusive) e inicio (inclusive)
-	int pivo_indice = (rand() % (fim - inicio + 1)) + inicio;
-	
-	// faz a troca para colocar o pivô no fim
-	troca(vet, pivo_indice, fim);
-	// chama a particiona
-	return particiona(vet, inicio, fim);
-}
-
-void quick_sort(int vet[], int inicio, int fim)
-{
-	if(inicio < fim)
-	{
-		// função particionar retorna o índice do pivô
-		int pivo_indice = particiona_random(vet, inicio, fim);
-		
-		// chamadas recursivas quick_sort
-		quick_sort(vet, inicio, pivo_indice - 1);
-		quick_sort(vet, pivo_indice + 1, fim);
-	}
-}
-
-int main()
-{
-    start() ;
-	// vetor que será ordenado
-	int vet[] = {19, 42, 20, 57, 53};
-	int tam_vet = sizeof(vet) / sizeof(int);
-	int i;
-	
-	// inicializa random seed
-	srand(time(NULL));
-	 
-	// chamada do quicksort
-	quick_sort(vet, 0, tam_vet - 1);
-
-	// mostra o vetor ordenado
-	return 0; 
-}
-/*
-#include <stdio.h>
-#include <stdlib.h> // srand, rand
-#include <time.h> // time
-
-
-void start ()
-{
-    int vet[] = {19, 42, 20, 57, 53};
-	int tam_vet = sizeof(vet) / sizeof(int);
-	int i;
-	srand(time(NULL));
-	quick_sort(vet, 0, tam_vet - 1);
-	
-	for(i = 0; i < tam_vet; i++)
-	    printf("%d ", vet[i]);
-}
-
-// função que realiza a troca entre dois elementos
-void troca(int vet[], int i, int j)
-{
-	int aux = vet[i];
-	vet[i] = vet[j];
-	vet[j] = aux;
-}
-
-// particiona e retorna o índice do pivô
-int particiona(int vet[], int inicio, int fim)
-{
-	int pivo, pivo_indice, i;
-	
-	pivo = vet[fim]; // o pivô é sempre o último elemento
-	pivo_indice = inicio;
-	
-	for(i = inicio; i < fim; i++)
-	{
-		// verifica se o elemento é <= ao pivô
-		if(vet[i] <= pivo)
-		{
-			// realiza a troca
-			troca(vet, i, pivo_indice);
-			// incrementa o pivo_indice
-			pivo_indice++;
-		}
-	}
-	
-	// troca o pivô
-	troca(vet, pivo_indice, fim);
-	
-	// retorna o índice do pivô
-	return pivo_indice;
-}
-
-// escolhe um pivô aleatório para evitar o pior caso do quicksort
-int particiona_random(int vet[], int inicio, int fim)
-{
-	// seleciona um número entre fim (inclusive) e inicio (inclusive)
-	int pivo_indice = (rand() % (fim - inicio + 1)) + inicio;
-	
-	// faz a troca para colocar o pivô no fim
-	troca(vet, pivo_indice, fim);
-	// chama a particiona
-	return particiona(vet, inicio, fim);
-}
-
-void quick_sort(int vet[], int inicio, int fim)
-{
-	if(inicio < fim)
-	{
-		// função particionar retorna o índice do pivô
-		int pivo_indice = particiona_random(vet, inicio, fim);
-		
-		// chamadas recursivas quick_sort
-		quick_sort(vet, inicio, pivo_indice - 1);
-		quick_sort(vet, pivo_indice + 1, fim);
-	}
-}
-
-int main()
-{
-    start() ;
-	// vetor que será ordenado
-	int vet[] = {19, 42, 20, 57, 53};
-	int tam_vet = sizeof(vet) / sizeof(int);
-	int i;
-	
-	// inicializa random seed
-	srand(time(NULL));
-	 
-	// chamada do quicksort
-	quick_sort(vet, 0, tam_vet - 1);
-
-	// mostra o vetor ordenado
-	return 0; 
-}*/
-ESSE AQUI :
-/*
-#include <stdio.h>
-#include <stdlib.h> // srand, rand
-#include <time.h> // time
-
-
-void start ()
-{
-    int tam_vet, i ;
-    int *vet, *vet2 ;
-    scanf ("%d", &tam_vet) ;
-    vet = malloc (tam_vet * sizeof(int)) ;
-    for (i = 0 ; i < tam_vet ; i++)
-    {
-        scanf ("%d", &vet[i]) ;
-    }
-    vet2 = malloc (tam_vet * sizeof(vet)) ;
-    for (i = 0 ; i < tam_vet ; i++) //copiando vetor para implementação do quicksort utiliziando a mediana
-    {
-        vet2[i] = vet[i] ;
-    }    
-    int c = 0 ;
-	quick_sort(vet, 0, tam_vet - 1, &c);
-	quick_sort(vet2, 0, tam_vet - 1, &c);
-	
-	for(i = 0; i < tam_vet; i++)
-	    printf("%d ", vet[i]);
-	printf ("\n\n") ;
-	for(i = 0; i < tam_vet; i++)
-	    printf("%d ", vet2[i]);
-}
-
-void troca(int vet[], int i, int j)
-{
-	int aux = vet[i];
-	vet[i] = vet[j];
-	vet[j] = aux;
-}
-
-int particiona(int vet[], int inicio, int fim)
-{
-	int pivo, pivo_indice, i ;
-	
-	pivo = vet[fim]; // o pivô é sempre o último elemento
-	pivo_indice = inicio;
-	
-	for(i = inicio; i < fim; i++)
-	{
-		// verifica se o elemento é <= ao pivô
-		if(vet[i] <= pivo)
-		{
-			// realiza a troca
-			troca(vet, i, pivo_indice);
-			// incrementa o pivo_indice
-			pivo_indice++;
-		}
-	}
-	
-	// troca o pivô
-	troca(vet, pivo_indice, fim);
-	
-	// retorna o índice do pivô
-	return pivo_indice;
-}
-
-// escolhe um pivô aleatório para evitar o pior caso do quicksort
-int particiona_random(int vet[], int inicio, int fim)
-{
-	// seleciona um número entre fim (inclusive) e inicio (inclusive)
-	int pivo_indice = (rand() % (fim - inicio + 1)) + inicio;
-	
-	// faz a troca para colocar o pivô no fim
-	troca(vet, pivo_indice, fim);
-	// chama a particiona
-	return particiona(vet, inicio, fim);
-}
-/*
-int particiona(int vet, int inicio, int fim)
-{
-  int pivot = array[fim];
-  int i = (inicio - 1);
-
-  for (int j = inicio; j < fim ; j++) {
-    if (vet[j] <= pivot) {
-        
-      i++;
-      
-      troca(vet, pivo_indice, fim);
-    }
-  }
-  troca(vet, pivo_indice, fim);
-  return (i + 1);
-}
-
-void quick_sort(int vet[], int inicio, int fim, *c) {
-  if (inicio < fim) {
-    int pivo_indice = partition(array, low, high);
-    quick_sort(vet, pivo_indice + 1, fim, c);
-    quick_sort(vet, pivo_indice + 1, fim, c);
-  }
-}
-
-*/
-
-void quick_sort(int vet[], int inicio, int fim, int *c)
-{
-	if(fim > inicio)
-	{
-		int pivo_indice = fim ;
-		if (*c == 0)
-		{
-		    pivo_indice = fim ;
-		    *c += 1 ;
-		}else
-		    pivo_indice = particiona_random(vet, inicio, fim);
-		quick_sort(vet, inicio, pivo_indice - 1, c);
-		quick_sort(vet, pivo_indice + 1, fim, c);
-	}
-}
-
-int main()
-{
-    start() ;
-	return 0; 
-}
-/*
-
-#include <stdio.h>
-#include <stdlib.h> // srand, rand
-#include <time.h> // time
-
-
-void start ()
-{
-    int tam_vet, i ;
-    int *vet, *vet2 ;
-    scanf ("%d", &tam_vet) ;
-    vet = malloc (tam_vet * sizeof(int)) ;
-    for (i = 0 ; i < tam_vet ; i++)
-    {
-        scanf ("%d", &vet[i]) ;
-    }
-    vet2 = malloc (tam_vet * sizeof(vet)) ;
-    for (i = 0 ; i < tam_vet ; i++) //copiando vetor para implementação do quicksort utiliziando a mediana
-    {
-        vet2[i] = vet[i] ;
-    }    
-    
-	quick_sort(vet, 0, tam_vet - 1);
-	quick_sort(vet2, 0, tam_vet - 1);
-	
-	for(i = 0; i < tam_vet; i++)
-	    printf("%d ", vet[i]);
-	printf ("\n\n") ;
-	for(i = 0; i < tam_vet; i++)
-	    printf("%d ", vet2[i]);
-}
-
-void troca(int vet[], int i, int j)
-{
-	int aux = vet[i];
-	vet[i] = vet[j];
-	vet[j] = aux;
-}
-
-int particiona(int vet[], int inicio, int fim)
-{
-	int pivo, pivo_indice, i;
-	
-	pivo = vet[fim]; // o pivô é sempre o último elemento
-	pivo_indice = inicio;
-	
-	for(i = inicio; i < fim; i++)
-	{
-		// verifica se o elemento é <= ao pivô
-		if(vet[i] <= pivo)
-		{
-			// realiza a troca
-			troca(vet, i, pivo_indice);
-			// incrementa o pivo_indice
-			pivo_indice++;
-		}
-	}
-	
-	// troca o pivô
-	troca(vet, pivo_indice, fim);
-	
-	// retorna o índice do pivô
-	return pivo_indice;
-}
-
-// escolhe um pivô aleatório para evitar o pior caso do quicksort
-int particiona_random(int vet[], int inicio, int fim)
-{
-	// seleciona um número entre fim (inclusive) e inicio (inclusive)
-	int pivo_indice = (rand() % (fim - inicio + 1)) + inicio;
-	
-	// faz a troca para colocar o pivô no fim
-	troca(vet, pivo_indice, fim);
-	// chama a particiona
-	return particiona(vet, inicio, fim);
-}
-
-void quick_sort(int vet[], int inicio, int fim)
-{
-	if(inicio < fim)
-	{
-		// função particionar retorna o índice do pivô
-		int pivo_indice = particiona_random(vet, inicio, fim);
-		
-		// chamadas recursivas quick_sort
-		quick_sort(vet, inicio, pivo_indice - 1);
-		quick_sort(vet, pivo_indice + 1, fim);
-	}
-}
-
-int main()
-{
-    start() ;
-	// vetor que será ordenado
-	int vet[] = {19, 42, 20, 57, 53};
-	int tam_vet = sizeof(vet) / sizeof(int);
-	int i;
-	
-	// inicializa random seed
-	srand(time(NULL));
-	 
-	// chamada do quicksort
-	quick_sort(vet, 0, tam_vet - 1);
-
-	// mostra o vetor ordenado
-	return 0; 
-}
-*/
-
-#include <stdio.h>
-#include <stdlib.h> // srand, rand
-#include <time.h> // time
-
-
-void start ()
-{
-    int tam_vet, i ;
-    int *vet, *vet2 ;
-    
-    scanf ("%d", &tam_vet) ;
-    vet = malloc (tam_vet * sizeof(int)) ;
-    for (i = 0 ; i < tam_vet ; i++)
-    {
-        scanf ("%d", &vet[i]) ;
-    }
-    vet2 = malloc (tam_vet * sizeof(vet)) ;
-    for (i = 0 ; i < tam_vet ; i++) //copiando vetor para implementação do quicksort utiliziando a mediana
-    {
-        vet2[i] = vet[i] ;
-    }    
-	quick_sort(vet, 0, tam_vet - 1);
-	quick_sort(vet2, 0, tam_vet - 1);
-	
-	for(i = 0; i < tam_vet; i++)
-	    printf("%d ", vet[i]);
-	printf ("\n\n") ;
-	for(i = 0; i < tam_vet; i++)
-	    printf("%d ", vet2[i]);
-}
+//https://www.ime.usp.br/~pf/analise_de_algoritmos/aulas/quick.html
 
 void troca(int *a, int *b)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
+    int t = *a ;
+    *a = *b ;
+    *b = t ;
 }
 
-int divide (int vet[], int p, int r)
+void start ()
 {
-	int pivo, i, j ;
-	pivo = vet[r]; // o pivô
-	i = p;
-	
-	for(j = p ; j < r ; j++)
-	{
-		if(vet[j] <= pivo)
-		{
-			troca(&vet[j], &vet[i]); //realizando a troca
-			i++;
-		}
-	}
-	troca(&vet[j], &vet[i]);
-	return i ; //retorna o indice do pivô
-}
-
-void quick_sort(int vet[], int p, int r)
-{
-    if (r > p)
+    int tam_vet, i ;
+    int *vet, *vet2 ;
+    
+    scanf ("%d", &tam_vet) ;
+    vet = malloc (tam_vet * sizeof(int)) ;
+    for (i = 0 ; i < tam_vet ; i++)
     {
-        int indice = divide(vet, p, r);
-        quick_sort(vet, p, indice - 1);
-        quick_sort(vet, indice + 1, r);
+        scanf ("%d", &vet[i]) ;
+    }
+    vet2 = malloc (tam_vet * sizeof(vet)) ;
+    for (i = 0 ; i < tam_vet ; i++) //copiando vetor para implementação do quicksort utiliziando a mediana
+    {
+        vet2[i] = vet[i] ;
+    }
+    int test = 0 ;
+	quick_sort(vet, 0, tam_vet - 1, test);
+	test += 1 ;
+	quick_sort(vet2, 0, tam_vet - 1, test);
+	
+	for(i = 0; i < tam_vet; i++) //print para auxiliar
+	    printf("%d ", vet[i]);
+	printf ("\n\n") ;
+	for(i = 0; i < tam_vet; i++)
+	    printf("%d ", vet2[i]);
+}
+
+int divide(int vet[], int p, int r)
+{
+    int pivo = vet[r];
+    int i = (p - 1);
+    for (int j = p ; j < r ; j++)
+    {
+        if (vet[j] < pivo)
+        {
+            troca(&vet[++i], &vet[j]);
+        }
+    }
+    troca(&vet[i + 1], &vet[r]);
+    return (i + 1);
+}
+
+int mediana(int vet[], int p, int r)
+{
+    int pivo ;
+    int mid = (p + r) / 2 ;
+    
+    if (vet[mid] < vet[p]) 
+        troca(&vet[mid], &vet[p]) ;
+    
+    if (vet[r] < vet[p])
+        troca(&vet[r], &vet[p]) ;
+    
+    if (vet[r] < vet[mid])
+        troca(&vet[r], &vet[mid]) ;
+    
+    troca(&vet[mid], &vet[r-1]) ;
+    pivo = vet[r-1] ;
+    
+    return divide(vet, p, r);
+}
+ 
+void quick_sort(int vet[], int p, int r, int test)
+{
+    if (p < r) 
+    {
+        int pi ;
+        if (test == 0)
+        {
+            pi = divide(vet, p, r);
+            //ordenando :
+            quick_sort(vet, p, pi - 1, test) ; //elementos a esquerda do pivo
+            quick_sort(vet, pi + 1, r, test) ; //elementos a direita do pivo
+            printf ("Executou com o pivo sendo ultimo elemento\n") ;
+        }else
+        if (test > 0)
+        {
+            pi = mediana(vet, p, r) ;
+            //ordenando :
+            quick_sort(vet, p, pi - 1, test) ; //elementos a esquerda do pivo
+            quick_sort(vet, pi + 1, r, test) ; //elementos a direita do pivo
+            printf ("Executou com o pivo sendo a mediana dos elementos\n") ;
+        }
     }
 }
 
